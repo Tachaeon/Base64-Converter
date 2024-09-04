133c2072c052da76b606bb48ade619690cee685c
James Orner <tachaeon@gmail.com>
2024-09-04 14:35:40 -0400

    123

--- Get-Base64.ps1
+++ Get-Base64.ps1
@@ -100,7 +100,7 @@ $form.Controls.Add($localfilebutton)
 
 $localfilebutton.Add_Click({
         $fileDialog = New-Object System.Windows.Forms.OpenFileDialog
-        $fileDialog.Filter = "PNG files (*.png)|*.png|GIF files (*.gif)|*.gif|PDF files (*.pdf)|*.pdf|ZIP files (*.zip)|*.zip|Executable files (*.exe)|*.exe|All files (*.*)|*.*"
+        $fileDialog.Filter = "ICO Files (*.ico)|*.ico|PNG files (*.png)|*.png|GIF files (*.gif)|*.gif|PDF files (*.pdf)|*.pdf|ZIP files (*.zip)|*.zip|Executable files (*.exe)|*.exe|All files (*.*)|*.*"
         if ($fileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
             $localfiletextBox.Text = $fileDialog.FileName
         }
