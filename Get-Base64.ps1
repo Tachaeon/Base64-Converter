6db6c2a33d1743e47419bf31a6113266c161da0a
James Orner <Tachaeon@gmail.com>
2024-11-14 15:32:42 -0500

    added drag and drop to form/removed old fields/buttons.

--- Get-Base64.ps1
+++ Get-Base64.ps1
@@ -60,7 +60,7 @@ function Save-FileFromBase64 {
 }
 
 function Base64 {
-    $Base64_Code = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("$File"))
+    $Base64_Code = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("$Files"))
     Set-Clipboard -Value $Base64_Code
     [System.Windows.Forms.MessageBox]::Show("Base64 sent to clipboard.", "Base64 --> Clipboard", 'OK', 'Information')
 }
@@ -74,123 +74,66 @@ $IconBytes = [Convert]::FromBase64String($IconBase64)
 $ims = New-Object IO.MemoryStream($IconBytes, 0, $IconBytes.Length)
 $ims.Write($IconBytes, 0, $IconBytes.Length)
 
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "Base64 Converter"
-$form.Size = New-Object System.Drawing.Size(390, 140)
+$form.Size = New-Object System.Drawing.Size(280, 140)
 $form.Icon = [System.Drawing.Icon]::FromHandle((New-Object System.Drawing.Bitmap -Argument $ims).GetHIcon())
 $form.StartPosition = "CenterScreen"
 $form.FormBorderStyle = "FixedDialog"
 
-$filelabel = New-Object System.Windows.Forms.Label
-$filelabel.Text = "File:"
-$filelabel.Location = New-Object System.Drawing.Point(10, 14)
-$filelabel.Size = New-Object System.Drawing.Size(30, 20)
-$form.Controls.Add($filelabel)
+$dropPanel = New-Object System.Windows.Forms.Panel
+$dropPanel.Location = New-Object System.Drawing.Point(10, 10)
+$dropPanel.Size = New-Object System.Drawing.Size(257, 50)
+$dropPanel.BackColor = [System.Drawing.Color]::LightGray
+$dropPanel.AllowDrop = $true
+$dropPanel.BorderStyle = 'FixedSingle'
+$dropPanelText = New-Object System.Windows.Forms.Label
+$dropPanelText.Text = "Drag and Drop File Here"
+$dropPanelText.AutoSize = $true
+$dropPanelText.Location = New-Object System.Drawing.Point(70, 20)
+$dropPanel.Controls.Add($dropPanelText)
+$form.Controls.Add($dropPanel)
 
-$localfiletextBox = New-Object System.Windows.Forms.TextBox
-$localfiletextBox.Location = New-Object System.Drawing.Point(45, 11)
-$localfiletextBox.Size = New-Object System.Drawing.Size(250, 20)
-$form.Controls.Add($localfiletextBox)
+$dropPanel.Add_DragEnter({
+    param ($sender, $e)
+    if ($e.Data.GetDataPresent([Windows.Forms.DataFormats]::FileDrop)) {
+        $e.Effect = [System.Windows.Forms.DragDropEffects]::Copy
+    }
+})
 
-$localfilebutton = New-Object System.Windows.Forms.Button
-$localfilebutton.Text = "Browse"
-$localfilebutton.Location = New-Object System.Drawing.Point(302, 10)
-$localfilebutton.Size = New-Object System.Drawing.Size(75, 20)
-$form.Controls.Add($localfilebutton)
-
-$localfilebutton.Add_Click({
-        $fileDialog = New-Object System.Windows.Forms.OpenFileDialog
-        $fileDialog.Filter = "ICO Files (*.ico)|*.ico|PNG files (*.png)|*.png|GIF files (*.gif)|*.gif|PDF files (*.pdf)|*.pdf|ZIP files (*.zip)|*.zip|Executable files (*.exe)|*.exe|All files (*.*)|*.*"
-        if ($fileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
-            $localfiletextBox.Text = $fileDialog.FileName
+$dropPanel.Add_DragDrop({
+    param ($sender, $e)
+    $files = $e.Data.GetData([Windows.Forms.DataFormats]::FileDrop)
+    if ($files.Length -gt 0) {
+            try {
+                Base64
+            }
+            catch {
+                Get-Error
+            }
         }
-    })
-
-$urltextbox = New-Object System.Windows.Forms.TextBox
-$urltextbox.Location = New-Object System.Drawing.Point(45, 41)
-$urltextbox.Size = New-Object System.Drawing.Size(250, 20)
-$form.Controls.Add($urltextbox)
-
-$urllabel = New-Object System.Windows.Forms.Label
-$urllabel.Text = "URL:"
-$urllabel.Location = New-Object System.Drawing.Point(10, 45)
-$urllabel.Size = New-Object System.Drawing.Size(100, 20)
-$form.Controls.Add($urllabel)
+})
 
 $frombase64Button = New-Object System.Windows.Forms.Button
 $frombase64Button.Text = "From: Base64"
-$frombase64Button.Location = New-Object System.Drawing.Point(90, 72)
+$frombase64Button.Location = New-Object System.Drawing.Point(90, 70)
 $frombase64Button.Size = New-Object System.Drawing.Size(100, 30)
 $form.Controls.Add($frombase64Button)
 
 $frombase64Button.Add_Click({
-        try {
-            $base64String = Get-Clipboard
-            $outputDirectory = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
-            Save-FileFromBase64 -Base64String $base64String -OutputDirectory $outputDirectory
-            [System.Windows.Forms.MessageBox]::Show("You file has been saved here:`n`n$outputDirectory", "Clipboard --> Base64", 'OK', 'Information')
-        }
-        catch {
-            Get-Error
-        }
-    })
-
-$tobase64button = New-Object System.Windows.Forms.Button
-$tobase64button.Text = "To: Base64"
-$tobase64button.Location = New-Object System.Drawing.Point(200, 72)
-$tobase64button.Size = New-Object System.Drawing.Size(100, 30)
-$form.Controls.Add($tobase64button)
-
-$tobase64button.Add_Click({
-        if ([string]::IsNullOrWhiteSpace($localfiletextBox.text) -and [string]::IsNullOrWhiteSpace($urltextBox.text)) {
-            [System.Windows.Forms.MessageBox]::Show("Please select a file or enter a URL.", "Oops!", 'OK', 'Information')
-            Return
-        }
-        if ($localfiletextBox.text) {
-            try {
-                $File = $localfiletextBox.text
-                Base64
-            }
-            catch {
-                Get-Error
-            }
-        }
-        if ($urltextBox.text) {
-            try {
-                $ProgressPreference = 'SilentlyContinue'
-                $Directory = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
-                $Filename = "Base64.tmp"
-                $WebClient = New-Object System.Net.WebClient 
-                $URL = $urltextBox.text
-                $File = "$Directory\$Filename" 
-                $WebClient.DownloadFile($URL, $File)
-                Base64
-            }
-            catch {
-                Get-Error
-            }
-        }
-    })
-
-$Help_Image = New-Object System.Windows.Forms.Label
-$IconBase64 = 'iVBORw0KGgoAAAANSUhEUgAAABkAAAAZCAYAAADE6YVjAAAAxklEQVRIS8WW0Q2AIAxEZTz/HcKRHMJ/x9NAgkGE3h0Y9JfS1zsK1U3ENy/rWQs79s2hFGaAlTxPbMGKECU5A3tBegARmKt6QL4AlEA3BAFKnrN7KIh1qEznBQgTaLUp2i9B0mSpOghBvjKXEOVwKGA4pNYAqFBJSUsbeye6IEhBtLoZwgJkJX6Dt0wBBAi6jGhWoHVflAxRldwQVg17y/OXmHogS883cy6xqLHzJFbLVMgcdhrzz4xPK1BUyX8ruR1oXiD7LvmhkjZLp5jjAAAAAElFTkSuQmCC'
-$IconBytes = [Convert]::FromBase64String($IconBase64)
-$Stream = [System.IO.MemoryStream]::new($IconBytes, 0, $IconBytes.Length)
-$Custom_Bookmark_Help_Picture = [System.Drawing.Icon]::FromHandle(([System.Drawing.Bitmap]::new($Stream).GetHIcon()))
-$ResizedImage = $Custom_Bookmark_Help_Picture.ToBitmap().GetThumbnailImage(20, 20, $null, [System.IntPtr]::Zero)
-$Help_Image.Image = $ResizedImage
-$Help_Image.Size = New-Object System.Drawing.Size(20, 20)
-$Help_Image.Location = New-Object System.Drawing.Point(60, 77)
-$Help_Image.BringToFront()
-$form.Controls.Add($Help_Image)
-
-$Help_Image.Add_Click({
-        [System.Windows.Forms.MessageBox]::Show("Copy the Base64 code into your clipboard then click the `"From: Base64`" button.", "Help", 'OK', 'Information')
-    })
-
-$windowcode = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
-$asyncwindow = Add-Type -MemberDefinition $windowcode -Name Win32ShowWindowAsync -Namespace Win32Functions -PassThru
-$null = $asyncwindow::ShowWindowAsync((Get-Process -PID $pid).MainWindowHandle, 0)
+    try {
+        $base64String = Get-Clipboard
+        $outputDirectory = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+        Save-FileFromBase64 -Base64String $base64String -OutputDirectory $outputDirectory
+        [System.Windows.Forms.MessageBox]::Show("Your file has been saved here:`n`n$outputDirectory", "Clipboard --> Base64", 'OK', 'Information')
+    }
+    catch {
+        Get-Error
+    }
+})
 
 $form.Add_Shown({ $form.Activate() })
 [void]$form.ShowDialog()
